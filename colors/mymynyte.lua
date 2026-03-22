61366ddbc2bb9c8487a03979a4ebc7a7d102a918
nik-kotine <mswv280407@gmail.com>
2026-03-22 02:08:57 -0500

    feat: add StatusLine :colorscheme support

--- colors/mymynyte.lua
+++ colors/mymynyte.lua
@@ -1,1 +1,5 @@
+vim.cmd("hi clear") -- clears existing highlighting groups to default colors
+vim.cmd("syntax reset") -- reinitializes syntax highlighting after clearing
+vim.g.colors_name = "mymynyte" -- registers active colorscheme name
+
 require("mymynyte").setup()
